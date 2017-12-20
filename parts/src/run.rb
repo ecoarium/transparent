3244ea694cdac3d33e97dc30ea6dbebf914ab639
Jay Flowers <jay.flowers@gmail.com>
2017-12-20 11:48:18 -0500

    fix env var name

--- parts/src/run.rb
+++ parts/src/run.rb
@@ -3,6 +3,6 @@ begin
   Clear.new.load_free if ENV['__TP_OAT'].nil? or ENV['__TP_OAT'].downcase == 'true'  
 rescue Exception => e
   $stderr.puts "#{e.message}
-#{e.backtrace.join("\n")}" if !ENV['__TP_GEB'].nil? and ENV['__TP_GEB'].downcase == 'true'
+#{e.backtrace.join("\n")}" if !ENV['<%= ENV['transparent_debug_env_name'] %>'].nil? and ENV['<%= ENV['transparent_debug_env_name'] %>'].downcase == 'true'
   exit -1
 end
